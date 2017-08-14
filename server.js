f1b18aa3a660f6b395fdd8cfda9c86e4425df253
leoworm <leo.charles.p@gmail.com>
2017-08-14 19:12:42 +0200

    fixed button

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var port = 80;
 var users = {leo: 'test', leopold: 'test1'}
 
 server.listen(port, function(){
-  console.log('Listening on port: ${port}')
+  console.log('Listening on port: ' + port)
 });
 
 app.get('/', function(req, res){
@@ -22,31 +22,32 @@ app.get('/', function(req, res){
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/login.html');
 });
-app.get('/js/lib/socket.io.js', function(req, res){
+app.get('./js/lib/socket.io.js', function(req, res){
   res.sendFile('/js/lib/socket.io.js');
 });
-app.get('/js/lib/jquery-3.2.1.min.js', function(req, res){
+app.get('./js/lib/jquery-3.2.1.min.js', function(req, res){
   res.sendFile('/js/lib/jquery-3.2.1.min.js');
 });
-app.get('/js/main.js', function(req, res){
+app.get('./js/main.js', function(req, res){
   res.sendFile('/js/main.js');
 });
-app.get('/css/main.css', function(req, res){
+app.get('./css/main.css', function(req, res){
   res.sendFile('/css/main.css');
 });
-app.get('/css/login.css', function(req, res){
+app.get('./css/login.css', function(req, res){
   res.sendFile('/css/login.css');
 });
 
 //login request received
-app.post('/login', function(req, res){
+app.post('/login.html', function(req, res){
   console.log('post received');
   /*if(hash == crypto.createHash('md5').update(req.body.pass).digest('hex')){
     res.sendFile(__dirname + '/main.html');
   }*/
+  console.log(req)
   var username = req.body.user
   var userpass = req.body.pass
-  console.log(username = userpass)
+  console.log(username + userpass)
 
   /*if(username != nil){
     if(crypto.createHash('md5').update(userpass))
